463ec11c40631870088fbe227163e47bb35c360a
Chiwenite Chilz <chilzchiwenite@gmail.com>
2026-06-24 14:22:44 -0700

    Reset database schema

--- app.py
+++ app.py
@@ -357,4 +357,9 @@ with app.app_context():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+
+
     app.run(debug=True)
